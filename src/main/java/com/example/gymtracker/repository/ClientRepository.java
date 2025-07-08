5a866b2b2be5c044c16da83044466a4fc0c52255
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-08 21:36:42 +0300

    feat: Добавить слой доступа к данным (репозитоии)

---
+++ src/main/java/com/example/gymtracker/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.gymtracker.repository;
+
+import com.example.gymtracker.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
