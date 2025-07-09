133b743c0ecdd37f62b6d202ac5ebd9658dd26c3
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-09 23:10:50 +0300

    feat: добавить слой сервиса для Client

---
+++ src/main/java/com/example/gymtracker/exception/ClientNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.gymtracker.exception;
+
+public class ClientNotFoundException extends RuntimeException{
+    public ClientNotFoundException(String s) {
+    }
+}
---
+++ src/main/java/com/example/gymtracker/service/impl/ClientServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.gymtracker.service.impl;
+
+import com.example.gymtracker.dto.request.RequestClientDto;
+import com.example.gymtracker.dto.response.ResponseClientDto;
+import com.example.gymtracker.exception.ClientNotFoundException;
+import com.example.gymtracker.mapper.ClientMapper;
+import com.example.gymtracker.model.Client;
+import com.example.gymtracker.repository.ClientRepository;
+import com.example.gymtracker.service.EntityService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ClientServiceImpl implements EntityService<Long, RequestClientDto, ResponseClientDto> {
+
+    private final ClientMapper mapper;
+    private final ClientRepository repository;
+
+    @Override
+    public ResponseClientDto save(RequestClientDto dto) {
+        Client client = mapper.toClient(dto);
+        return mapper.fromClient(repository.save(client));
+    }
+    @Override
+    public ResponseClientDto getById(Long id) {
+        return mapper.fromClient(getClientOrThrow(id));
+    }
+    @Override
+    public List<ResponseClientDto> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::fromClient)
+                .toList();
+    }
+    @Override
+    public void deleteById(Long entityId) {
+        getClientOrThrow(entityId);
+        repository.deleteById(entityId);
+    }
+    @Override
+    public ResponseClientDto update(Long id, RequestClientDto updatedDto) {
+        Client existingClient = getClientOrThrow(id);
+        mapper.updateFromClientDto(updatedDto, existingClient);
+        repository.save(existingClient);
+        return mapper.fromClient(existingClient);
+    }
+    private Client getClientOrThrow(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Client not found with id:" + id));
+    }
+}
