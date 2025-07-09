12e73fc812a93f1b436f395fbf8eec64a7b583e4
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-09 23:06:09 +0300

    feat: добавить Mapper
    * Добавлено
        - Зависимось Mapstruct в pom.xml
        - Доблен интерфейс ClientMapper для маппинга из RequestClientDto в Client

---
+++ src/main/java/com/example/gymtracker/mapper/ClientMapper.java
@@ -0,0 +1,21 @@
+package com.example.gymtracker.mapper;
+
+import com.example.gymtracker.dto.request.RequestClientDto;
+import com.example.gymtracker.dto.response.ResponseClientDto;
+import com.example.gymtracker.model.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ClientMapper {
+    ResponseClientDto fromClient(Client client);
+
+    @Mapping(target = "workouts", ignore = true)
+    Client toClient(RequestClientDto requestClientDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "workouts", ignore = true)
+    void updateFromClientDto(RequestClientDto sourceDto, @MappingTarget Client toUpdateClient);
+}
