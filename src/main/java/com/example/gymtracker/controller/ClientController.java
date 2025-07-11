f19f3f452a5359d6bc84ea8a77f28bec6d76d067
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-11 22:07:09 +0300

    feat: добавить возможность обновлять Client

--- src/main/java/com/example/gymtracker/controller/ClientController.java
+++ src/main/java/com/example/gymtracker/controller/ClientController.java
@@ -3,12 +3,13 @@ package com.example.gymtracker.controller;
 import com.example.gymtracker.dto.request.RequestClientDto;
 import com.example.gymtracker.dto.response.ResponseClientDto;
 import com.example.gymtracker.service.ClientService;
-import com.example.gymtracker.service.impl.ClientServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -36,7 +37,15 @@ public class ClientController {
 
 
     @PostMapping(consumes = "application/json")
-    public ResponseEntity<ResponseClientDto> saveClient(@RequestBody RequestClientDto clientDto) {
-        return new ResponseEntity<>(clientService.save(clientDto), HttpStatus.CREATED);
+    public ResponseEntity<ResponseClientDto> saveClient(@RequestBody RequestClientDto clientDto, UriComponentsBuilder componentsBuilder) {
+        ResponseClientDto savedClient = clientService.save(clientDto);
+        URI location = componentsBuilder.path("/api/clients/{id}")
+                .buildAndExpand(savedClient.id()).toUri();
+        return ResponseEntity.created(location).body(savedClient);
+    }
+
+    @PutMapping(value = "/{id}", consumes = "application/json")
+    public ResponseEntity<ResponseClientDto> updateClient(@PathVariable Long id, @RequestBody RequestClientDto clientDto) {
+        return ResponseEntity.ok(clientService.update(id, clientDto));
     }
 }
