ed97a1a3041d82568799fd6b80c154174faff3f0
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-19 22:08:01 +0300

    feat: добавить endpoints для CRUD операций с тренировками клиента
    - createWorkout
      * сохранение новой тренировки клиента
    - deleteClientWorkout
      * удаление тренировки пользователя по Id
    - updateClientWorkout
      * полное обновление данных тренировки
    - patchClientWorkout
      * частичное обновление данных тренировки

--- src/main/java/com/example/gymtracker/controller/ClientWorkoutsController.java
+++ src/main/java/com/example/gymtracker/controller/ClientWorkoutsController.java
@@ -1,12 +1,17 @@
 package com.example.gymtracker.controller;
 
+import com.example.gymtracker.dto.request.RequestWorkoutDto;
 import com.example.gymtracker.dto.response.ResponseWorkoutDto;
 import com.example.gymtracker.service.ClientWorkoutService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/clients/{clientId}/workouts", produces = "application/json")
@@ -23,9 +28,9 @@ public class ClientWorkoutsController {
     }
 
     @GetMapping("/{workoutId}")
-    public ResponseEntity<?> getWorkoutByIdAndClientId(@PathVariable("clientId") Long clientId,
-                                                       @PathVariable("workoutId") Long workoutId,
-                                                       @RequestParam(required = false, defaultValue = "default") String detail) {
+    public ResponseEntity<?> getClientWorkoutById(@PathVariable("clientId") Long clientId,
+                                                  @PathVariable("workoutId") Long workoutId,
+                                                  @RequestParam(required = false, defaultValue = "default") String detail) {
 
         return switch (detail) {
             case "full" -> ResponseEntity.ok()
@@ -35,5 +40,38 @@ public class ClientWorkoutsController {
             default -> ResponseEntity.ok()
                     .body(clientWorkoutService.workoutsByClientId(clientId));
         };
+    }
+
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<ResponseWorkoutDto> createWorkout(@PathVariable("clientId") Long clientId,
+                                                            @RequestBody RequestWorkoutDto requestWorkoutDto) {
+        ResponseWorkoutDto createdWorkout = clientWorkoutService.createWorkout(clientId, requestWorkoutDto);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/id")
+                .buildAndExpand(createdWorkout.id())
+                .toUri();
+        return ResponseEntity.created(location).body(createdWorkout);
+    }
+
+    @DeleteMapping("/{workoutId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteWorkout(@PathVariable("clientId") Long clientId,
+                              @PathVariable("workoutId") Long workoutId) {
+        clientWorkoutService.deleteClientWorkout(clientId, workoutId);
+    }
+
+    @PutMapping(value = "/{workoutId}", consumes = "application/json")
+    public ResponseEntity<ResponseWorkoutDto> updateClientWorkout(@PathVariable("workoutId") Long workoutId,
+                                                                  @PathVariable("clientId") Long clientId,
+                                                                  @RequestBody RequestWorkoutDto requestWorkoutDto) {
+        return ResponseEntity.ok(clientWorkoutService.updateWorckout(clientId, workoutId, requestWorkoutDto));
+    }
+
+    @PatchMapping(value = "/{workoutId}", consumes = "application/json")
+    public ResponseEntity<ResponseWorkoutDto> updateClientWorkout(@PathVariable("workoutId") Long workoutId,
+                                                                  @PathVariable("clientId") Long clientId,
+                                                                  Map<String, Object> updates) {
+        return ResponseEntity.ok(clientWorkoutService.patchWorckout(clientId, workoutId, updates));
     }
 }
