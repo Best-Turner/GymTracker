32455a6a426c31033fa6016c649f710be46806ee
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-07-28 19:35:34 +0300

    feat: добавть mapper для ExerciseSet

--- src/main/java/com/example/gymtracker/mapper/ExerciseSetMapper.java
+++ src/main/java/com/example/gymtracker/mapper/ExerciseSetMapper.java
@@ -4,24 +4,62 @@ import com.example.gymtracker.dto.request.RequestExercisePatchDto;
 import com.example.gymtracker.dto.request.RequestExerciseSetDto;
 import com.example.gymtracker.dto.response.ExerciseSetDetailsResponse;
 import com.example.gymtracker.dto.response.ExerciseSetShortResponse;
+import com.example.gymtracker.model.Exercise;
 import com.example.gymtracker.model.ExerciseSet;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import com.example.gymtracker.model.Workout;
+import org.mapstruct.*;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ExerciseSetMapper extends EntityMapper<RequestExerciseSetDto, ExerciseSet, ExerciseSetShortResponse> {
 
-    //TODO написать реализацию
     @Override
     ExerciseSetShortResponse toDto(ExerciseSet exerciseSet);
 
     @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "workout", ignore = true)
+    @Mapping(source = "exerciseId", target = "exercise", expression = "java(idToExercise(exerciseSetDto.exerciseId()))")
     ExerciseSet toEntity(RequestExerciseSetDto exerciseSetDto);
 
+    @Mapping(source = "exercise", target = "exerciseName", qualifiedByName = "exerciseToExerciseName")
+    @Mapping(source = "exercise", target = "muscleGroup", expression = "java(exerciseToString(exerciseSet.exercise()))")
+    @Mapping(target = "workoutId", expression = "java(workoutToId(exerciseSet.getWorkout()))")
     ExerciseSetDetailsResponse toDetailsEntity(ExerciseSet exerciseSet);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "exerciseId", target = "exercise", qualifiedByName = "mapExerciseIdToExercise")
+    void updateExerciseSet(RequestExerciseSetDto requestExerciseSetDto, @MappingTarget ExerciseSet exerciseSet);
 
-    void updateExerciseSet(RequestExerciseSetDto requestExerciseSetDto, ExerciseSet exerciseSet);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "exercise", ignore = true)
+    @Mapping(target = "workout", ignore = true)
+    void patchExerciseSet(RequestExercisePatchDto requestExercisePatch, @MappingTarget ExerciseSet exerciseSet);
 
-    void patchExerciseSet(RequestExercisePatchDto requestExercisePatch, ExerciseSet exerciseSet);
+    default Exercise idToExercise(Long id) {
+        return Exercise.builder().id(id).build();
+    }
+
+    @Named("exerciseToExerciseName")
+    default String exerciseToExerciseName(ExerciseSet exerciseSet) {
+        Exercise exercise = exerciseSet.getExercise();
+        return exercise == null ? "<< не указано >>" : exercise.getName();
+    }
+
+    default String exerciseToString(Exercise exercise) {
+        return (exercise == null || exercise.getMuscleGroup() == null) ?
+                "<< не указано >>" :
+                exercise.getMuscleGroup().getDescription();
+    }
+
+    default Long workoutToId(Workout workout) {
+        return workout.getId();
+    }
+
+    @Named("mapExerciseIdToExercise")
+    default Exercise mapExerciseIdToExercise(Long exerciseId) {
+        return exerciseId == null ? null :
+                Exercise.builder()
+                        .id(exerciseId)
+                        .build();
+    }
 }
